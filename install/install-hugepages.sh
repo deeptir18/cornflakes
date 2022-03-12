cf787d586de7ab8095b215ceed688dd87511c440
Deepti Raghavan <deeptir@cs.stanford.edu>
2022-03-12 15:33:49 -0700

    Install huge pages

--- install/install-hugepages.sh
+++ install/install-hugepages.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
-NRHUGEPAGES=1
+NRHUGEPAGES=$1
 HUGEPGSZ=`cat /proc/meminfo  | grep Hugepagesize | cut -d : -f 2 | tr -d ' '`
-
-# clear current huge pages
-clear_huge_pages
-# reserve new huge pages
-echo "echo $NRHUGEPAGES > /sys/kernel/mm/hugepages/hugepages-${HUGEPGSZ}/nr_hugepages" > .echo_tmp
-echo "Reserving $NRHUGEPAGES on each node"
-for d in /sys/devices/system/node/node? ; do
-	node=$(basename $d)
-	echo "echo $NRHUGEPAGES > $d/hugepages/hugepages-${HUGEPGSZ}/nr_hugepages" >> .echo_tmp
-	done
-	echo "Reserving hugepages"
-done
-sudo sh .echo_tmp
-rm -f .echo_tmp
-
-create_mnt_huge
 
 #
 # Removes all reserved hugepages.
@@ -63,4 +47,18 @@ remove_mnt_huge()
 		sudo rm -R /mnt/huge
 	fi
 }
+# clear current huge pages
+clear_huge_pages
+# reserve new huge pages
+echo "echo $NRHUGEPAGES > /sys/kernel/mm/hugepages/hugepages-${HUGEPGSZ}/nr_hugepages" > .echo_tmp
+echo "Reserving $NRHUGEPAGES on each node of size ${HUGEPGSZ}"
+for d in /sys/devices/system/node/node? ; do
+	node=$(basename $d)
+	echo "echo $NRHUGEPAGES > $d/hugepages/hugepages-${HUGEPGSZ}/nr_hugepages" >> .echo_tmp
+done
+sudo sh .echo_tmp
+rm -f .echo_tmp
+
+create_mnt_huge
+
 
