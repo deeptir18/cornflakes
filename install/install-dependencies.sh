24e6cb4764848f19e5131c86c9d8e20e0d264ff4
Deepti Raghavan <deeptir@cs.stanford.edu>
2022-09-20 10:59:45 -0400

    Update install-dependencies.sh
    Add bazel install

--- install/install-dependencies.sh
+++ install/install-dependencies.sh
@@ -11,6 +11,7 @@ sudo apt install apt-transport-https curl gnupg
 curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor bazel-archive-keyring.gpg
 sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+sudo apt update && sudo apt install bazel
 
 
 sudo apt-get install -y quilt chrpath graphviz swig libnl-route-3-200 libnl-route-3-dev dpatch libnl-3-dev
