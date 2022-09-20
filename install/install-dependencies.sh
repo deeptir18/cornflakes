7025e5c9d51f277867b9cc83f191a2949342980a
Deepti Raghavan <deeptir@cs.stanford.edu>
2022-09-20 11:12:57 -0400

    Update install-dependencies.sh

--- install/install-dependencies.sh
+++ install/install-dependencies.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y libbsd-dev libelf-dev libpcap-dev
 
 # Required for protobuf
 sudo apt install apt-transport-https curl gnupg
-curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor bazel-archive-keyring.gpg
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
 sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
 sudo apt update && sudo apt install bazel
