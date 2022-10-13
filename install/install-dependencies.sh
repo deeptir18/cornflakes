0b919f74c4f4f2967b6466d36ba14c647de0c755
Gina Yuan <gina.yuan@gmail.com>
2022-10-12 18:41:40 -0600

    Add -y to apt install scripts

--- install/install-dependencies.sh
+++ install/install-dependencies.sh
@@ -7,11 +7,11 @@ sudo apt-get update
 sudo apt-get install -y libbsd-dev libelf-dev libpcap-dev
 
 # Required for protobuf
-sudo apt install apt-transport-https curl gnupg
+sudo apt install -y apt-transport-https curl gnupg
 curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
 sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
-sudo apt update && sudo apt install bazel
+sudo apt update && sudo apt install -y bazel
 
 
 sudo apt-get install -y quilt chrpath graphviz swig libnl-route-3-200 libnl-route-3-dev dpatch libnl-3-dev
