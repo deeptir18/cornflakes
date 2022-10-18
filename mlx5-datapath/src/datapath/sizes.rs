40550fbf8452e82d62cfaf94e9d72a60d2e80044
Deepti Raghavan <deeptir@cs.stanford.edu>
2022-10-18 11:26:54 -0600

    Mlx5 datapath: allow recv of buffers > 8192

--- mlx5-datapath/src/datapath/sizes.rs
+++ mlx5-datapath/src/datapath/sizes.rs
@@ -25,7 +25,7 @@ use color_eyre::eyre::{bail, Result};
 use cornflakes_libos::mem;
 
 pub const RX_MEMPOOL_DATA_PGSIZE: usize = mem::PGSIZE_2MB;
-pub const RX_MEMPOOL_DATA_LEN: usize = 8192;
+pub const RX_MEMPOOL_DATA_LEN: usize = 16384;
 pub const RX_MEMPOOL_MIN_NUM_ITEMS: usize = 8192;
 
 pub fn align_up(x: usize, align_size: usize) -> usize {
