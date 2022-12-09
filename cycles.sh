3143bdd697e12b34db4386adbad0ec9ab1079445
Gina Yuan <gina.yuan@gmail.com>
2022-12-09 10:20:12 -0700

    Update cycles rate

--- cycles.sh
+++ cycles.sh
@@ -28,15 +28,17 @@ else
 fi
 
 if [ $2 = "cfkv" ]; then
+	rate=500000
 	if [ $3 = "server" ]; then
 		cmd="sudo env LD_LIBRARY_PATH=/mydata/ygina/cornflakes/dpdk-datapath/3rdparty/dpdk/build/lib/x86_64-linux-gnu /mydata/ygina/cornflakes/target/release/ycsb_mlx5 --config_file $CORNFLAKES_CONFIG --mode server --server_ip 192.168.1.1 --debug_level info --trace $YCSB_LOAD_TRACE --num_values 2 --num_keys 1 --value_size UniformOverSizes-2048 --push_buf_type $push_buf_type --inline_mode $inline_mode --serialization $serialization"
 	elif [ $3 = "client" ]; then
-		cmd="sudo env LD_LIBRARY_PATH=/mydata/ygina/cornflakes/dpdk-datapath/3rdparty/dpdk/build/lib/x86_64-linux-gnu /mydata/ygina/cornflakes/target/release/ycsb_dpdk --config_file $CORNFLAKES_CONFIG --server_ip 192.168.1.1 --our_ip 192.168.1.2 --debug_level info --mode client --queries $YCSB_ACCESS_TRACE --trace $YCSB_LOAD_TRACE --client_id 0 --num_clients 1 --num_threads 16 --time 25 --rate 50000 --value_size UniformOverSizes-2048 --num_values 2 --num_keys 1 --push_buf_type $push_buf_type --inline_mode $inline_mode --serialization $serialization"
+		cmd="sudo env LD_LIBRARY_PATH=/mydata/ygina/cornflakes/dpdk-datapath/3rdparty/dpdk/build/lib/x86_64-linux-gnu /mydata/ygina/cornflakes/target/release/ycsb_dpdk --config_file $CORNFLAKES_CONFIG --server_ip 192.168.1.1 --our_ip 192.168.1.2 --debug_level info --mode client --queries $YCSB_ACCESS_TRACE --trace $YCSB_LOAD_TRACE --client_id 0 --num_clients 1 --num_threads 16 --time 25 --rate $rate --value_size UniformOverSizes-2048 --num_values 2 --num_keys 1 --push_buf_type $push_buf_type --inline_mode $inline_mode --serialization $serialization"
 	else
 		echo $usage
 		exit 1
 	fi
 else
+	rate=50000
 	if [ $2 = "echo1" ]; then
 		message="single"
 	elif [ $2 = "echo2" ]; then
@@ -49,7 +51,7 @@ else
 	if [ $3 = "server" ]; then
 		cmd="sudo env LD_LIBRARY_PATH=/mydata/ygina/cornflakes/dpdk-datapath/3rdparty/dpdk/build/lib/x86_64-linux-gnu nice -n -19 taskset -c 2 /mydata/ygina/cornflakes/target/release/ds_echo_mlx5 --config_file $CORNFLAKES_CONFIG --mode server --server_ip 192.168.1.1 --debug_level info --message $message --size 4096 --push_buf_type $push_buf_type --inline_mode $inline_mode --serialization $serialization"
 	elif [ $3 = "client" ]; then
-		cmd="sudo env RUST_BACKTRACE=1 LD_LIBRARY_PATH=/mydata/ygina/cornflakes/dpdk-datapath/3rdparty/dpdk/build/lib/x86_64-linux-gnu /mydata/ygina/cornflakes/target/release/ds_echo_dpdk --mode client --config_file $CORNFLAKES_CONFIG --server_ip 192.168.1.1 --our_ip 192.168.1.2 --debug_level info --time 15 --rate 100000 --num_threads 16 --message $message --size 4096 --push_buf_type $push_buf_type --inline_mode $inline_mode --serialization $serialization"
+		cmd="sudo env RUST_BACKTRACE=1 LD_LIBRARY_PATH=/mydata/ygina/cornflakes/dpdk-datapath/3rdparty/dpdk/build/lib/x86_64-linux-gnu /mydata/ygina/cornflakes/target/release/ds_echo_dpdk --mode client --config_file $CORNFLAKES_CONFIG --server_ip 192.168.1.1 --our_ip 192.168.1.2 --debug_level info --time 15 --rate $rate --num_threads 16 --message $message --size 4096 --push_buf_type $push_buf_type --inline_mode $inline_mode --serialization $serialization"
 	else
 		echo $usage
 		exit 1
