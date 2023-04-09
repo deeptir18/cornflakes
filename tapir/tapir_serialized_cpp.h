4b3bd3c8c636bd14982b852dd202131989160e88
ShreyaRavi <sravi2@stanford.edu>
2023-04-09 16:26:24 -0700

    finish ffi layer for ReplyConsensusMessage'

--- tapir/tapir_serialized_cpp.h
+++ tapir/tapir_serialized_cpp.h
@@ -20,7 +20,7 @@ void ReplyInconsistentMessage_set_view(void *self_, uint64_t view);
 
 void ReplyInconsistentMessage_get_replicaIdx(void *self_, uint32_t *return_ptr);
 
-void ReplyInconsistentMessage_set_replicaIdx(void *self_, uint32_t replicaIdx);
+void ReplyInconsistentMessage_set_replicaIdx(void *self_, uint32_t replica_idx);
 
 void ReplyInconsistentMessage_get_finalized(void *self_, uint32_t *return_ptr);
 
@@ -58,7 +58,7 @@ void ConfirmMessage_set_view(void *self_, uint64_t view);
 
 void ConfirmMessage_get_replicaIdx(void *self_, uint32_t *return_ptr);
 
-void ConfirmMessage_set_replicaIdx(void *self_, uint32_t replicaIdx);
+void ConfirmMessage_set_replicaIdx(void *self_, uint32_t replica_idx);
 
 void ConfirmMessage_get_mut_opid(void *self_, void **return_ptr);
 
@@ -75,5 +75,35 @@ uint32_t Mlx5Connection_ConfirmMessage_queue_cornflakes_arena_object(void *self_
                                                                      bool end_batch);
 
 void ConfirmMessage_free(const void *self_);
+
+void ReplyConsensusMessage_new_in(void *arena, void **return_ptr);
+
+void ReplyConsensusMessage_get_view(void *self_, uint64_t *return_ptr);
+
+void ReplyConsensusMessage_set_view(void *self_, uint64_t view);
+
+void ReplyConsensusMessage_get_replicaIdx(void *self_, uint32_t *return_ptr);
+
+void ReplyConsensusMessage_set_replicaIdx(void *self_, uint32_t replica_idx);
+
+void ReplyConsensusMessage_get_finalized(void *self_, uint32_t *return_ptr);
+
+void ReplyConsensusMessage_set_finalized(void *self_, uint32_t finalized);
+
+void ReplyConsensusMessage_get_mut_opid(void *self_, void **return_ptr);
+
+uint32_t ReplyConsensusMessage_deserialize(void *self_,
+                                           const void *data,
+                                           uintptr_t data_len,
+                                           uintptr_t offset,
+                                           void *arena);
+
+uint32_t Mlx5Connection_ReplyConsensusMessage_queue_cornflakes_arena_object(void *self_,
+                                                                            uint32_t msg_id,
+                                                                            uintptr_t conn_id,
+                                                                            void *cornflakes_obj,
+                                                                            bool end_batch);
+
+void ReplyConsensusMessage_free(const void *self_);
 
 } // extern "C"
