d289b77d55cfb0ab7d7f24dc733dffb815c962d3
Shreya Ravi <sravi2@stanford.edu>
2023-04-09 16:23:20 -0700

    ffi layer for ReplyConsensusMessage

--- tapir/src/lib.rs
+++ tapir/src/lib.rs
@@ -280,7 +280,7 @@ pub extern "C" fn ConfirmMessage_get_mut_opid<'registered>(
     self_: *mut ::std::os::raw::c_void,
     return_ptr: *mut *mut ::std::os::raw::c_void,
 ) {
-    let mut self_ = unsafe { Box::from_raw(self_ as *mut ReplyInconsistentMessage<'registered, Mlx5Connection>) };
+    let mut self_ = unsafe { Box::from_raw(self_ as *mut ConfirmMessage<'registered, Mlx5Connection>) };
     let value: *mut OpID<'registered, Mlx5Connection> = self_.get_mut_opid();
     unsafe { *return_ptr = value as _ };
     Box::into_raw(self_);
@@ -339,6 +339,150 @@ pub extern "C" fn Mlx5Connection_ConfirmMessage_queue_cornflakes_arena_object<'a
 #[inline]
 #[no_mangle]
 pub extern "C" fn ConfirmMessage_free<'arena>(self_: *const ::std::os::raw::c_void) {
-    let _ = unsafe { Box::from_raw(self_ as *mut ReplyInconsistentMessage<'arena, Mlx5Connection>) };
+    let _ = unsafe { Box::from_raw(self_ as *mut ConfirmMessage<'arena, Mlx5Connection>) };
 }
 
+// ReplyConsensusMessage
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn ReplyConsensusMessage_new_in<'arena>(
+    arena: *mut ::std::os::raw::c_void,
+    return_ptr: *mut *mut ::std::os::raw::c_void,
+) {
+    let arg0 = arena as *const bumpalo::Bump;
+    let value = ReplyConsensusMessage::<'arena, Mlx5Connection>::new_in(unsafe { &*arg0 });
+    let value = Box::into_raw(Box::new(value));
+    unsafe { *return_ptr = value as _ };
+}
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn ReplyConsensusMessage_get_view<'registered>(
+    self_: *mut ::std::os::raw::c_void,
+    return_ptr: *mut u64,
+) {
+    let self_ = unsafe { Box::from_raw(self_ as *mut ReplyConsensusMessage<'registered, Mlx5Connection>) };
+    let value = self_.get_view();
+    unsafe { *return_ptr = value };
+    Box::into_raw(self_);
+}
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn ReplyConsensusMessage_set_view<'registered>(self_: *mut ::std::os::raw::c_void, view: u64) {
+    let mut self_ =
+        unsafe { Box::from_raw(self_ as *mut ReplyConsensusMessage<'registered, Mlx5Connection>) };
+    self_.set_view(view);
+    Box::into_raw(self_);
+}
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn ReplyConsensusMessage_get_replicaIdx<'registered>(
+    self_: *mut ::std::os::raw::c_void,
+    return_ptr: *mut u32,
+) {
+    let self_ = unsafe { Box::from_raw(self_ as *mut ReplyConsensusMessage<'registered, Mlx5Connection>) };
+    let value = self_.get_replicaIdx();
+    unsafe { *return_ptr = value };
+    Box::into_raw(self_);
+}
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn ReplyConsensusMessage_set_replicaIdx<'registered>(self_: *mut ::std::os::raw::c_void, replicaIdx: u32) {
+    let mut self_ =
+        unsafe { Box::from_raw(self_ as *mut ReplyConsensusMessage<'registered, Mlx5Connection>) };
+    self_.set_replicaIdx(replicaIdx);
+    Box::into_raw(self_);
+}
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn ReplyConsensusMessage_get_finalized<'registered>(
+    self_: *mut ::std::os::raw::c_void,
+    return_ptr: *mut u32,
+) {
+    let self_ = unsafe { Box::from_raw(self_ as *mut ReplyConsensusMessage<'registered, Mlx5Connection>) };
+    let value = self_.get_finalized();
+    unsafe { *return_ptr = value };
+    Box::into_raw(self_);
+}
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn ReplyConsensusMessage_set_finalized<'registered>(self_: *mut ::std::os::raw::c_void, finalized: u32) {
+    let mut self_ =
+        unsafe { Box::from_raw(self_ as *mut ReplyConsensusMessage<'registered, Mlx5Connection>) };
+    self_.set_finalized(finalized);
+    Box::into_raw(self_);
+}
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn ReplyConsensusMessage_get_mut_opid<'registered>(
+    self_: *mut ::std::os::raw::c_void,
+    return_ptr: *mut *mut ::std::os::raw::c_void,
+) {
+    let mut self_ = unsafe { Box::from_raw(self_ as *mut ReplyConsensusMessage<'registered, Mlx5Connection>) };
+    let value: *mut OpID<'registered, Mlx5Connection> = self_.get_mut_opid();
+    unsafe { *return_ptr = value as _ };
+    Box::into_raw(self_);
+}
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn ReplyConsensusMessage_deserialize<'arena>(
+    self_: *mut ::std::os::raw::c_void,
+    data: *const ::std::os::raw::c_void,
+    data_len: usize,
+    offset: usize,
+    arena: *mut ::std::os::raw::c_void,
+) -> u32 {
+    let mut self_ = unsafe { Box::from_raw(self_ as *mut ReplyConsensusMessage<'arena, Mlx5Connection>) };
+    let data_slice = unsafe { std::slice::from_raw_parts(data as _, data_len as _) };
+    let arg1 = offset;
+    let arg2 = arena as *const bumpalo::Bump;
+    let value = self_.deserialize_from_raw(data_slice, arg1, unsafe { &*arg2 });
+    //let value = self_.deserialize(unsafe { &*arg0 }, arg1, unsafe { &*arg2 });
+    match value {
+        Ok(value) => value,
+        Err(_) => {
+            return 1;
+        }
+    }
+    Box::into_raw(self_);
+    0
+}
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn Mlx5Connection_ReplyConsensusMessage_queue_cornflakes_arena_object<'arena>(
+    self_: *mut ::std::os::raw::c_void,
+    msg_id: u32,
+    conn_id: usize,
+    cornflakes_obj: *mut ::std::os::raw::c_void,
+    end_batch: bool,
+) -> u32 {
+    let mut self_ = unsafe { Box::from_raw(self_ as *mut Mlx5Connection) };
+    let arg0 = msg_id;
+    let arg1 = conn_id;
+    let arg2 = unsafe { *Box::from_raw(cornflakes_obj as *mut ReplyConsensusMessage<'arena, Mlx5Connection>) };
+    let arg3 = end_batch;
+    let value = self_.queue_cornflakes_arena_object(arg0, arg1, arg2, arg3);
+    match value {
+        Ok(value) => value,
+        Err(_) => {
+            return 1;
+        }
+    }
+    Box::into_raw(self_);
+    0
+}
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn ReplyConsensusMessage_free<'arena>(self_: *const ::std::os::raw::c_void) {
+    let _ = unsafe { Box::from_raw(self_ as *mut ReplyConsensusMessage<'arena, Mlx5Connection>) };
+}
