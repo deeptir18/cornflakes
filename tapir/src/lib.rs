358d6f7c3bae0f8c43b98050997af3ea3bfb4b8a
Shreya Ravi <sravi2@stanford.edu>
2023-04-09 18:41:01 -0700

    add UnloggedReplyMessage to tapir ffi

--- tapir/src/lib.rs
+++ tapir/src/lib.rs
@@ -511,6 +511,8 @@ pub extern "C" fn ReplyConsensusMessage_free<'arena>(self_: *const ::std::os::ra
     let _ = unsafe { Box::from_raw(self_ as *mut ReplyConsensusMessage<'arena, Mlx5Connection>) };
 }
 
+// CFBytes
+
 #[inline]
 #[no_mangle]
 pub extern "C" fn CFBytes_new_in(
@@ -559,4 +561,119 @@ pub extern "C" fn CFBytes_unpack(
     unsafe { *return_ptr = ptr.as_ptr() };
     unsafe { *return_len_ptr = self_.len() };
     Box::into_raw(self_);
+}
+
+// ReplyConsensusMessage
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn UnloggedReplyMessage_new_in<'arena>(
+    arena: *mut ::std::os::raw::c_void,
+    return_ptr: *mut *mut ::std::os::raw::c_void,
+) {
+    let arg0 = arena as *const bumpalo::Bump;
+    let value = UnloggedReplyMessage::<'arena, Mlx5Connection>::new_in(unsafe { &*arg0 });
+    let value = Box::into_raw(Box::new(value));
+    unsafe { *return_ptr = value as _ };
+}
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn UnloggedReplyMessage_get_clientreqid<'registered>(
+    self_: *mut ::std::os::raw::c_void,
+    return_ptr: *mut u64,
+) {
+    let self_ = unsafe { Box::from_raw(self_ as *mut UnloggedReplyMessage<'registered, Mlx5Connection>) };
+    let value = self_.get_clientreqid();
+    unsafe { *return_ptr = value };
+    Box::into_raw(self_);
+}
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn UnloggedReplyMessage_set_clientreqid<'registered>(self_: *mut ::std::os::raw::c_void, clientreqid: u64) {
+    let mut self_ =
+        unsafe { Box::from_raw(self_ as *mut UnloggedReplyMessage<'registered, Mlx5Connection>) };
+    self_.set_clientreqid(clientreqid);
+    Box::into_raw(self_);
+}
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn UnloggedReplyMessage_get_reply<'registered>(
+    self_: *mut ::std::os::raw::c_void,
+    return_ptr: *mut *mut ::std::os::raw::c_void,
+) {
+    let self_ = unsafe { Box::from_raw(self_ as *mut UnloggedReplyMessage<'registered, Mlx5Connection>) };
+    let value: *const CFBytes<Mlx5Connection> = self_.get_reply();
+    unsafe { *return_ptr = value as _ };
+    Box::into_raw(self_);
+}
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn UnloggedReplyMessage_set_reply<'registered>(
+    self_: *mut ::std::os::raw::c_void,
+    val: *const ::std::os::raw::c_void,
+) {
+    let mut self_ = unsafe { Box::from_raw(self_ as *mut UnloggedReplyMessage<'registered, Mlx5Connection>) };
+    let arg0 = unsafe { *Box::from_raw(val as *mut CFBytes<Mlx5Connection>) };
+    self_.set_reply(arg0);
+    Box::into_raw(self_);
+}
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn UnloggedReplyMessage_deserialize<'arena>(
+    self_: *mut ::std::os::raw::c_void,
+    data: *const ::std::os::raw::c_void,
+    data_len: usize,
+    offset: usize,
+    arena: *mut ::std::os::raw::c_void,
+) -> u32 {
+    let mut self_ = unsafe { Box::from_raw(self_ as *mut UnloggedReplyMessage<'arena, Mlx5Connection>) };
+    let data_slice = unsafe { std::slice::from_raw_parts(data as _, data_len as _) };
+    let arg1 = offset;
+    let arg2 = arena as *const bumpalo::Bump;
+    let value = self_.deserialize_from_raw(data_slice, arg1, unsafe { &*arg2 });
+    //let value = self_.deserialize(unsafe { &*arg0 }, arg1, unsafe { &*arg2 });
+    match value {
+        Ok(value) => value,
+        Err(_) => {
+            return 1;
+        }
+    }
+    Box::into_raw(self_);
+    0
+}
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn Mlx5Connection_UnloggedReplyMessage_queue_cornflakes_arena_object<'arena>(
+    self_: *mut ::std::os::raw::c_void,
+    msg_id: u32,
+    conn_id: usize,
+    cornflakes_obj: *mut ::std::os::raw::c_void,
+    end_batch: bool,
+) -> u32 {
+    let mut self_ = unsafe { Box::from_raw(self_ as *mut Mlx5Connection) };
+    let arg0 = msg_id;
+    let arg1 = conn_id;
+    let arg2 = unsafe { *Box::from_raw(cornflakes_obj as *mut UnloggedReplyMessage<'arena, Mlx5Connection>) };
+    let arg3 = end_batch;
+    let value = self_.queue_cornflakes_arena_object(arg0, arg1, arg2, arg3);
+    match value {
+        Ok(value) => value,
+        Err(_) => {
+            return 1;
+        }
+    }
+    Box::into_raw(self_);
+    0
+}
+
+#[inline]
+#[no_mangle]
+pub extern "C" fn UnloggedReplyMessage_free<'arena>(self_: *const ::std::os::raw::c_void) {
+    let _ = unsafe { Box::from_raw(self_ as *mut UnloggedReplyMessage<'arena, Mlx5Connection>) };
 }
