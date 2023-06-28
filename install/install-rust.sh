30c55ca29b33d21a2f5e687b7d30717463218a65
Deepti Raghavan <deeptir@cs.stanford.edu>
2023-06-28 14:31:29 -0600

    Install Rust to specific version

--- install/install-rust.sh
+++ install/install-rust.sh
@@ -1,4 +1,6 @@
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source "$HOME/.cargo/env"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+rustup install 1.70.0
+rustup default 1.70.0
 rustup component add rustfmt
+rustup default stable
 
