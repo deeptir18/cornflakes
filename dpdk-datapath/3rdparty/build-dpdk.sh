b9f4b2b056ba23fa17ba6da2e96118dba9c70db4
deeptir@cs.stanford.edu <deeptir@cs.stanford.edu>
2023-07-11 16:21:57 -0700

    Updates for dpdk ice compilation

--- dpdk-datapath/3rdparty/build-dpdk.sh
+++ dpdk-datapath/3rdparty/build-dpdk.sh
@@ -8,7 +8,7 @@
 DPDK=$1 # has to be a full path
 pushd $DPDK
 meson build 
-meson configure -Ddisable_drivers=net/mlx4,common/mlx4 build
+meson configure -Ddisable_drivers=net/mlx4,common/mlx4,regex/cn9k build
 meson configure -Dprefix=$DPDK/build build
 ninja -C build
 ninja -C build install
